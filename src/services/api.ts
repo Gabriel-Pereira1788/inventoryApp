76be7cb44d9e40a1998403c082c4095ea87f4bc6
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2023-01-27 00:11:36 -0300

    refact: types global

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import {BASE_URL} from '@env';
 
 export const api = axios.create({
-  baseURL: 'https://server-system-inventory-production.up.railway.app/',
+  baseURL: BASE_URL,
 });
